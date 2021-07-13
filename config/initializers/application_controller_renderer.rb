6e5587dd15e3c59e7b0883b86c55e2d3646c069d
38takumi <saitout485@gmail.com>
2021-07-13 07:02:24 +0000

    sample_app finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f293f80ddc7081f7359c07b5a7a76b31a14988e052fd37ebec90a5b5447a83527d91a31abf11c3f04b79e6455d2384a5b3c2a969a7a218c75ae5330f5da9698f'
